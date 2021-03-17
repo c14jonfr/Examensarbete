e4a583c3f70522f5182ee0fc0353896e97e4f71c
c14jonfr <c14jonfr@student.his.se>
2021-03-17 11:49:37 +0100

    moved navigation HTML to new file nav-menu.php which can be reused for all pages

---
+++ nav-menu.php
@@ -0,0 +1,79 @@
+<?php echo '
+  <nav role="navigation">
+  <ul class="top-menu">
+    <li tabindex="0">Cars
+      <ul class="sub-menu">
+        <li tabindex="0">Company A
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company B
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company C
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li tabindex="0">Boats
+      <ul class="sub-menu">
+        <li tabindex="0">Company A
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company B
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company C
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li tabindex="0">Planes
+      <ul class="sub-menu sub-menu-right">
+        <li tabindex="0">Company A
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company B
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+        <li tabindex="0">Company C
+          <ul class="sub-sub-menu">
+            <li><a href="#">Blue</a></li>
+            <li><a href="#">Red</a></li>
+            <li><a href="#">Green</a></li>
+          </ul>
+        </li>
+      </ul>
+</nav>
+'?>
