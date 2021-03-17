d791e6373aa42009844f3885a180f90fa065f826
c14jonfr <c14jonfr@student.his.se>
2021-03-17 12:32:12 +0100

    updated shared nav-menu.php with correct relative paths

--- Shared/nav-menu.php
+++ Shared/nav-menu.php
@@ -5,23 +5,23 @@
       <ul class="sub-menu">
         <li tabindex="0">Company A
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./CarABlue.php">Blue</a></li>
+            <li><a href="./CarARed.php">Red</a></li>
+            <li><a href="./CarAGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company B
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./CarBBlue.php">Blue</a></li>
+            <li><a href="./CarBRed.php">Red</a></li>
+            <li><a href="./CarBGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company C
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./CarCBlue.php">Blue</a></li>
+            <li><a href="./CarCRed.php">Red</a></li>
+            <li><a href="./CarCGreen.php">Green</a></li>
           </ul>
         </li>
       </ul>
@@ -30,23 +30,23 @@
       <ul class="sub-menu">
         <li tabindex="0">Company A
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./BoatABlue.php">Blue</a></li>
+            <li><a href="./BoatARed.php">Red</a></li>
+            <li><a href="./BoatAGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company B
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./BoatBBlue.php">Blue</a></li>
+            <li><a href="./BoatBRed.php">Red</a></li>
+            <li><a href="./BoatBGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company C
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./BoatCBlue.php">Blue</a></li>
+            <li><a href="./BoatCRed.php">Red</a></li>
+            <li><a href="./BoatCGreen.php">Green</a></li>
           </ul>
         </li>
       </ul>
@@ -55,23 +55,23 @@
       <ul class="sub-menu sub-menu-right">
         <li tabindex="0">Company A
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./PlaneABlue.php">Blue</a></li>
+            <li><a href="./PlaneARed.php">Red</a></li>
+            <li><a href="./PlaneAGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company B
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./PlaneBBlue.php">Blue</a></li>
+            <li><a href="./PlaneBRed.php">Red</a></li>
+            <li><a href="./PlaneBGreen.php">Green</a></li>
           </ul>
         </li>
         <li tabindex="0">Company C
           <ul class="sub-sub-menu">
-            <li><a href="#">Blue</a></li>
-            <li><a href="#">Red</a></li>
-            <li><a href="#">Green</a></li>
+            <li><a href="./PlaneCBlue.php">Blue</a></li>
+            <li><a href="./PlaneCRed.php">Red</a></li>
+            <li><a href="./PlaneCGreen.php">Green</a></li>
           </ul>
         </li>
       </ul>
