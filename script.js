da3f8818396b22b790e328d9ad3127a86e842934
c14jonfr <c14jonfr@student.his.se>
2021-03-19 14:23:54 +0100

    added basic functionality for showing/hiding simulation as well as toggling between simulations in demo page

---
+++ script.js
@@ -0,0 +1,13 @@
+const VIS = document.getElementById('visual-impairment-simulator');
+const sims = ['none','macular-degeneration', 'glaucoma', 'hemianopia', 'cataracts'];
+let currentSim = 0;
+
+const toggleVIS = () => {
+  VIS.classList.toggle('hidden');
+}
+
+const nextSim = () => {
+  VIS.classList.remove(sims[currentSim]);
+  currentSim = currentSim == sims.length-1 ? 0 : currentSim+1;
+  VIS.classList.add(sims[currentSim])
+}
