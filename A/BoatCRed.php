d70222111f427f26c60c508fd1a3a71227c24432
c14jonfr <c14jonfr@student.his.se>
2021-03-22 14:51:33 +0100

    added product content for all product pages

--- A/BoatCRed.php
+++ A/BoatCRed.php
@@ -11,7 +11,19 @@
   <div class="container">
     <?php include '../Shared/nav-menu.php'?>
     <main>
-      <img src="../Shared/Images/BoatRed.jpg" alt="productimage boat company C red">  
+      <div class="product">
+        <img src="../Shared/Images/BoatRed.jpg" alt="productimage boat company C red">
+        <h3 class="product-name">Red Boat</h3>
+        <h4 class="product-company">Company C</h4>
+        <div class="buttons">
+          <button class="buy-button">Buy</button>
+          <button class="share-button">Recommend to a friend</button>
+        </div>
+        <p class="product-description">
+        A boat is a watercraft of a large range of types and sizes, but generally smaller than a ship, which is distinguished by its larger size, shape, cargo or passenger capacity, or its ability to carry boats.
+        Small boats are typically found on inland waterways such as rivers and lakes, or in protected coastal areas. However, some boats, such as the whaleboat, were intended for use in an offshore environment. In modern naval terms, a boat is a vessel small enough to be carried aboard a ship. Anomalous definitions exist, as lake freighters 1,000 feet (300 m) long on the Great Lakes are called "boats".
+        </p>
+      </div>
     </main>
   </div>
 </body>
--- A/CarABlue.php
+++ A/CarABlue.php
@@ -11,7 +11,19 @@
   <div class="container">
     <?php include '../Shared/nav-menu.php'?>
     <main>
-      <img src="../Shared/Images/CarBlue.jpg" alt="productimage car company A blue">  
+      <div class="product">
+        <img src="../Shared/Images/CarBlue.jpg" alt="productimage car company A blue">
+        <h3 class="product-name">Blue Car</h3>
+        <h4 class="product-company">Company A</h4>
+        <div class="buttons">
+          <button class="buy-button">Buy</button>
+          <button class="share-button">Recommend to a friend</button>
+        </div>
+        <p class="product-description">
+          A car (or automobile) is a wheeled motor vehicle used for transportation. Most definitions of cars say that they run primarily on roads, seat one to eight people, have four wheels, and mainly transport people rather than goods.
+          Cars came into global use during the 20th century, and developed economies depend on them. The year 1886 is regarded as the birth year of the modern car when German inventor Karl Benz patented his Benz Patent-Motorwagen. Cars became widely available in the early 20th century. One of the first cars accessible to the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company.
+        </p>
+      </div>
     </main>
   </div>
 </body>
