c715acd8075dd45b687be87d851e19952e4dde26
c14jonfr <c14jonfr@student.his.se>
2021-03-24 15:01:31 +0100

    added script-links and audio-element for audio handling for all pages in A-directory

--- A/CarBGreen.php
+++ A/CarBGreen.php
@@ -5,9 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../Shared/style.css">
+  <script src="./AudioHandler.js" defer></script>
   <title>Car Company B Green</title>
 </head>
 <body>
+  <audio src="" id="audio-element" loop></audio>
   <div class="container">
     <?php include '../Shared/nav-menu.php'?>
     <main>
