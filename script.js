69511b52dc68b5efd0257dcac31e37db78fd3778
c14jonfr <c14jonfr@student.his.se>
2021-03-24 14:27:00 +0100

    added basic functionality for showing instructions and handling task solutions

--- script.js
+++ script.js
@@ -39,31 +39,52 @@ audioItems.forEach(item => {
   })
 })
 
-
-let currentTask;
-if(localStorage.getItem('currentTask')){
-  currentTask = localStorage.getItem('currentTask');
-}else{
-  currentTask = 0;
-}
+//array of tasks, this should be updated with the real tasks
+const tasks = ['do this', 'do that']
+//gets current task if there is one saved in localstorage, otherwise start from 0
+let currentTask = parseInt(localStorage.getItem('currentTask')) || 0;
 
 const solutions = document.querySelectorAll('.solution');
+const startButton = document.getElementById('start-button');
+const instructions = document.getElementById('instructions');
+const instruction = document.getElementById('instruction-p');
+instruction.innerHTML = tasks[currentTask];
 
+//start the timer and hide the instructions
+const startTask = () => {
+  instructions.style.display = 'none';
+  const startTime = Date.now();
+  localStorage.setItem('taskStart', startTime);
+}
+startButton.addEventListener('click', startTask);
+
+//get saved taskTimes from localstorage, if none available set to empty array
+const taskTimes = JSON.parse(localStorage.getItem('taskTimes')) || [];
+
+//stops timer, saves time in localstorage, increments currentTask. If all the tasks are done redirects to questionnaire page, otherwise show instructions for next task
 const nextTask = () => {
-  console.log('go next')
-  //stop timer here, save to localstorage
+  const stopTime = Date.now();
+  const startTime = localStorage.getItem('taskStart');
+  const totalTime = (stopTime - startTime) / 1000;
+  const taskTime = {task: currentTask, time: totalTime}
+  taskTimes.push(taskTime);
+  localStorage.setItem('taskTimes', JSON.stringify(taskTimes))
   currentTask++;
+  currentTask > tasks.length - 1 && (window.location.href = '../questions.php');
   localStorage.setItem('currentTask', currentTask)
+  instruction.textContent = tasks[currentTask];
+  instructions.style.display = 'flex';
 }
 
+//if clicked item is solution for the task, complete task and go to next
 const handleSolutionClick = (e) => {
   const target = e.target.getAttribute('data-solution');
-  console.log(target, currentTask, target == currentTask)
   if(target == currentTask){
     nextTask();
   }
 }
 
+//eventlisteners for all solution-elements
 solutions.forEach(solution => {
   solution.addEventListener('click', handleSolutionClick);
 })
