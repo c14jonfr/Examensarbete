4c2db48515dd1fe05c346821a117fec1682b4975
c14jonfr <c14jonfr@student.his.se>
2021-03-26 12:28:40 +0100

    added FormHandler.js (adds tasktimes from localstorage before submitting) and a Form page (only for directory A for now, B will be added later)

---
+++ A/Form.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../Shared/style.css">
+  <script src="../script.js" defer></script>
+  <script src="../Shared/FormHandler.js" defer></script>
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+  <form name="form" method="post" id="form" action="./Thanks.php">
+        <div>
+            <label for="age">Age: </label>
+            <select name="age" id="age" required>
+              <option value="20-30">20-30</option>
+              <option value="31-40">31-40</option>
+              <option value="41-50">41-50</option>
+              <option value="51-60">51-60</option>
+              <option value="61-70">61-70</option>
+              <option value="71 or older">71 or older</option>
+            </select>
+        </div>
+        <div>
+            <label for="gender">Gender: </label>
+            <select name="gender" id="gender" required>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+        </div>
+        <div>
+            <label for="effort">How much effort did it take you to navigate with the menu and solve the tasks? </label><br>
+            0 <input type="range" name="effort" id="effort" min="0" max="10"> 10
+        </div>
+        <div>
+            <label for="annoyance">Did you find the audio annoying?</label><br>
+            Not annoying <input type="range" name="annoyance" id="annoyance" min="0" max="10"> Annoying
+        </div>
+        <div>
+            <label for="effort">Did you find the audio helpful?</label><br>
+            Not helpful <input type="range" name="helpfulness" id="helfulness" min="0" max="10"> Helpful
+        </div>
+        <div>
+          <input type="hidden" name="taskTimes" id="taskTimes" value="">
+        </div>
+        <div style="text-align: center;">
+          <input type="hidden" name="form_submitted" value="1" />
+          <input type="submit" value="Submit">
+        </div>
+    </form>
+  </div>
+</body>
+</html>
---
+++ Shared/FormHandler.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const taskTimes = localStorage.getItem('taskTimes');
+  const taskTimesInput = document.getElementById('taskTimes');
+  taskTimesInput.value = taskTimes;
+  form.submit();
+}
