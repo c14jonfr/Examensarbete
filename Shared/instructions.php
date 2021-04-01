0762dcaf271ec42234ade1b0ce298c0420176dd3
c14jonfr <c14jonfr@student.his.se>
2021-04-01 14:43:41 +0200

    added new shared files instructions.php

---
+++ Shared/instructions.php
@@ -0,0 +1,11 @@
+<?php echo '
+<div class="instructions" id="instructions">
+  <div class="instruction-div">
+    <p id="previous-task">This is your first task!</p>
+    <p id="next-task">Your next task is:</p>
+    <p id="instruction-p">here goes instruction/task text</p>
+    <p id="timer-info">The timer starts when you click the "Start task"-button</p>
+    <button id="start-button">Start task</button>
+  </div>
+</div>
+'?>
