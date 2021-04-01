e617736e7242ffbede729d1d2ba28c80df9dfd4d
c14jonfr <c14jonfr@student.his.se>
2021-04-01 11:43:24 +0200

    instructions are now not hidden after starting a task but minimized in top right corner

--- Shared/TaskHandler.js
+++ Shared/TaskHandler.js
@@ -13,14 +13,16 @@ const instructions = document.getElementById('instructions');
 const instruction = document.getElementById('instruction-p');
 instruction.innerHTML = tasks[currentTask];
 
-if(!localStorage.getItem('instructions-status') || localStorage.getItem('instructions-status') === 'shown'){
-  instructions.style.display = 'flex';
+if(!localStorage.getItem('instructions-status') || localStorage.getItem('instructions-status') === 'fullsize'){
+  instructions.classList.remove('instructions-minimized');
+}else{
+  instructions.classList.add('instructions-minimized');
 }
 
 //start the timer and hide the instructions
 const startTask = () => {
-  instructions.style.display = 'none';
-  localStorage.setItem('instructions-status', 'hidden');
+  instructions.classList.toggle('instructions-minimized')
+  localStorage.setItem('instructions-status', 'minimized');
   const startTime = Date.now();
   localStorage.setItem('taskStart', startTime);
 }
@@ -41,8 +43,9 @@ const nextTask = () => {
   currentTask > tasks.length - 1 && (window.location.href = './Form.php');
   localStorage.setItem('currentTask', currentTask)
   instruction.textContent = tasks[currentTask];
-  instructions.style.display = 'flex';
-  localStorage.setItem('instructions-status', 'shown');
+  //instructions.style.display = 'flex';
+  instructions.classList.toggle('instructions-minimized');
+  //localStorage.setItem('instructions-status', 'shown');
 }
 
 //if clicked item is solution for the task, complete task and go to next
