cfc1f639f7802c01d113abf2dbd8cf24a4fc132f
c14jonfr <c14jonfr@student.his.se>
2021-04-07 13:47:41 +0200

    added shared file introduction.php to be used on indexpages of both A and B applications

---
+++ Shared/introduction.php
@@ -0,0 +1,10 @@
+<?php echo '
+  <div class="introduction" id="introduction">
+  <h2>Read this!</h2>
+  <p>This application and test is part of a study being done as part of a bachelor\'s thesis. Participation is voluntary and can be stopped at any time during the test. No data will be submitted before the questionnaire at the end is submitted.</p>
+  <p>The test consists of 2 parts. Part 1 is a practical test where you will be given tasks to solve. These tasks requires you to navigate to the correct product page and click an object on the screen. When all tasks have been completed you will be redirected to part 2 which consists of a questionnaire about your experience during part 1.</p>
+  <p>During part 1 the time it takes to solve each task will be collected as well as the total time spent during the test (from start of first task until end of last task). Part 2 (the questionnaire) will collect data about your age and gender.</p>
+  <p>Please only use the pages navigation menu to navigate, <strong>don\'t</strong> use your web browsers back button.</p>
+  <button id="close-introduction">Go to part 1</button>
+</div>
+'?>
