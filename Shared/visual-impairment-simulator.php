ae7a00c39f9c3eb89213cb8d8d137e2bb1f93007
c14jonfr <c14jonfr@student.his.se>
2021-04-16 13:09:53 +0200

    made visual simulation (blur-filter and glaucoma) more drastic

--- Shared/visual-impairment-simulator.php
+++ Shared/visual-impairment-simulator.php
@@ -1,7 +1,7 @@
 <?php echo '
 <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <filter id="blur">
-    <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
+    <feGaussianBlur in="SourceGraphic" stdDeviation="3" />
   </filter>
 </svg>
 <div id="visual-impairment-simulator" class="vis-container glaucoma"></div>
