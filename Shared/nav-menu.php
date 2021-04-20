b5fd75fa38c795184e319bbc044087549049174b
c14jonfr <c14jonfr@student.his.se>
2021-04-20 14:41:31 +0200

    updated order of menu items

--- Shared/nav-menu.php
+++ Shared/nav-menu.php
@@ -28,25 +28,25 @@
     </li>
     <li tabindex="0" class="audio-item" data-audio="Boat.mp3">Boats
       <ul class="sub-menu">
-        <li tabindex="0" class="audio-item" data-audio="BoatA.mp3">Company A
-          <ul class="sub-sub-menu">
-            <li><a href="./BoatABlue.php" class="audio-item" data-audio="BoatABlue.mp3">Blue</a></li>
-            <li><a href="./BoatARed.php" class="audio-item" data-audio="BoatARed.mp3">Red</a></li>
-            <li><a href="./BoatAGreen.php" class="audio-item" data-audio="BoatAGreen.mp3">Green</a></li>
-          </ul>
-        </li>
         <li tabindex="0" class="audio-item" data-audio="BoatB.mp3">Company B
           <ul class="sub-sub-menu">
-            <li><a href="./BoatBBlue.php" class="audio-item" data-audio="BoatBBlue.mp3">Blue</a></li>
-            <li><a href="./BoatBRed.php" class="audio-item" data-audio="BoatBRed.mp3">Red</a></li>
             <li><a href="./BoatBGreen.php" class="audio-item" data-audio="BoatBGreen.mp3">Green</a></li>
+            <li><a href="./BoatBRed.php" class="audio-item" data-audio="BoatBRed.mp3">Red</a></li>
+            <li><a href="./BoatBBlue.php" class="audio-item" data-audio="BoatBBlue.mp3">Blue</a></li>
           </ul>
         </li>
         <li tabindex="0" class="audio-item" data-audio="BoatC.mp3">Company C
           <ul class="sub-sub-menu">
-            <li><a href="./BoatCBlue.php" class="audio-item" data-audio="BoatCBlue.mp3">Blue</a></li>
-            <li><a href="./BoatCRed.php" class="audio-item" data-audio="BoatCRed.mp3">Red</a></li>
             <li><a href="./BoatCGreen.php" class="audio-item" data-audio="BoatCGreen.mp3">Green</a></li>
+            <li><a href="./BoatCRed.php" class="audio-item" data-audio="BoatCRed.mp3">Red</a></li>
+            <li><a href="./BoatCBlue.php" class="audio-item" data-audio="BoatCBlue.mp3">Blue</a></li>
+          </ul>
+        </li>
+        <li tabindex="0" class="audio-item" data-audio="BoatA.mp3">Company A
+          <ul class="sub-sub-menu">
+            <li><a href="./BoatAGreen.php" class="audio-item" data-audio="BoatAGreen.mp3">Green</a></li>
+            <li><a href="./BoatARed.php" class="audio-item" data-audio="BoatARed.mp3">Red</a></li>
+            <li><a href="./BoatABlue.php" class="audio-item" data-audio="BoatABlue.mp3">Blue</a></li>
           </ul>
         </li>
       </ul>
@@ -54,27 +54,28 @@
     <!-- Original sample by JillianCallahan https://freesound.org/people/JillianCallahan/ used under Sampling+ License-->
     <li tabindex="0" class="audio-item" data-audio="Plane.mp3">Planes
       <ul class="sub-menu sub-menu-right">
+        <li tabindex="0" class="audio-item" data-audio="PlaneC.mp3">Company C
+          <ul class="sub-sub-menu">
+            <li><a href="./PlaneCRed.php" class="audio-item" data-audio="PlaneCRed.mp3">Red</a></li>
+            <li><a href="./PlaneCGreen.php" class="audio-item" data-audio="PlaneCGreen.mp3">Green</a></li>
+            <li><a href="./PlaneCBlue.php" class="audio-item" data-audio="PlaneCBlue.mp3">Blue</a></li>
+          </ul>
+        </li>
         <li tabindex="0" class="audio-item" data-audio="PlaneA.mp3">Company A
           <ul class="sub-sub-menu">
-            <li><a href="./PlaneABlue.php" class="audio-item" data-audio="PlaneABlue.mp3">Blue</a></li>
             <li><a href="./PlaneARed.php" class="audio-item" data-audio="PlaneARed.mp3">Red</a></li>
             <li><a href="./PlaneAGreen.php" class="audio-item" data-audio="PlaneAGreen.mp3">Green</a></li>
+            <li><a href="./PlaneABlue.php" class="audio-item" data-audio="PlaneABlue.mp3">Blue</a></li>
           </ul>
         </li>
         <li tabindex="0" class="audio-item" data-audio="PlaneB.mp3">Company B
           <ul class="sub-sub-menu">
-            <li><a href="./PlaneBBlue.php" class="audio-item" data-audio="PlaneBBlue.mp3">Blue</a></li>
             <li><a href="./PlaneBRed.php" class="audio-item" data-audio="PlaneBRed.mp3">Red</a></li>
             <li><a href="./PlaneBGreen.php" class="audio-item" data-audio="PlaneBGreen.mp3">Green</a></li>
+            <li><a href="./PlaneBBlue.php" class="audio-item" data-audio="PlaneBBlue.mp3">Blue</a></li>
           </ul>
         </li>
-        <li tabindex="0" class="audio-item" data-audio="PlaneC.mp3">Company C
-          <ul class="sub-sub-menu">
-            <li><a href="./PlaneCBlue.php" class="audio-item" data-audio="PlaneCBlue.mp3">Blue</a></li>
-            <li><a href="./PlaneCRed.php" class="audio-item" data-audio="PlaneCRed.mp3">Red</a></li>
-            <li><a href="./PlaneCGreen.php" class="audio-item" data-audio="PlaneCGreen.mp3">Green</a></li>
-          </ul>
-        </li>
+
       </ul>
   </nav>
 '?>
