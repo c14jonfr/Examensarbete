7f108d26899316a647582eebc6c5dcef07f7599b
c14jonfr <c14jonfr@student.his.se>
2021-04-20 14:20:14 +0200

    minor updates to introduction text and styling

--- Shared/introduction.php
+++ Shared/introduction.php
@@ -1,11 +1,12 @@
 <?php echo '
   <div class="introduction" id="introduction">
-  <h2>Read this!</h2>
-  <p>This application and test is part of a study being done as part of a bachelor\'s thesis. Participation is voluntary and can be stopped at any time during the test. No data will be submitted before the questionnaire at the end is submitted.</p>
-  <p>The test consists of 2 parts. Part 1 is a practical test where you will be given tasks to solve. These tasks require you to navigate to the correct product page and click an object on the screen. When all tasks have been completed you will be redirected to part 2 which consists of a questionnaire about your experience during part 1.</p>
-  <p>During part 1 (the practical test) the time it takes to solve each task will be collected as well as the total time spent during the test (from start of first task until end of last task). Part 2 (the questionnaire) will collect data about your age and gender as well as your answers about your experience during the practical test.</p>
-  <p>During part 1 (the practical test) the screen will simulate one or more visual impairment symptoms. These are merely simulations and are not necessarily representative of what having visual impairment is like.</p>
-  <p>To participate in this study you will need to enable sound on your computer. Whether you use headphones or speakers is up to you but headphones are recommended for the optimal experience. Please only use the pages navigation menu to navigate, <strong>don\'t</strong> use your web browsers back button.</p>
-  <button id="close-introduction">Go to part 1</button>
-</div>
+    <h2>Read this!</h2>
+    <p>This application and test is part of a study being done as part of a bachelor\'s thesis. Participation is voluntary and can be stopped at any time during the test. No data will be submitted before the questionnaire at the end is submitted.</p>
+    <p>The test consists of 2 parts. Part 1 is a practical test where you will be given tasks to solve. These tasks require you to navigate to the correct product page and click an object or element on the screen. When all tasks have been completed you will be redirected to part 2 which consists of a questionnaire about your experience during part 1.</p>
+    <p>During part 1 (the practical test) the time it takes to solve each task will be collected as well as the total time spent during the test (from start of first task until end of last task). The timer for each tasks starts when you click the "Start task"-button and ends when you click the correct object or element on the correct product page. Part 2 (the questionnaire) will collect data about your age and gender as well as your answers about your experience during the practical test.</p>
+    <p>During part 1 (the practical test) the screen will simulate one or more visual impairment symptoms. These are merely simulations and are not necessarily representative of what having visual impairment is like.</p>
+    <p>To participate in this study you will need to enable sound on your computer. Whether you use headphones or speakers is up to you but headphones are recommended for the optimal experience. Please only use the pages navigation menu to navigate, <strong>don\'t</strong> use your web browsers back button.</p>
+    <p>Please do your best to solve the tasks as fast as you can. The timer doesn\'t start until you click the "Start task"-button. Take your time to read the task instructions carefully.</p>
+    <button id="close-introduction">Go to part 1</button>
+  </div>
 '?>
