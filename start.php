3c45cc8131a394ff81487b1b9530c8a6aa36cba2
c14jonfr <c14jonfr@student.his.se>
2021-05-07 22:17:46 +0200

    HOTFIX: start.php script now redirects to A/B depending on how many answers from each application has been collected. Should make sure an even amount of answers from each application is collected

--- start.php
+++ start.php
@@ -2,7 +2,13 @@
   $file = "../Data/data.json";
   $strJsonFileContents = file_get_contents($file);
   $array = json_decode($strJsonFileContents, true);
-  if(count($array) % 2 == 0){
+  $A = 0;
+  $B = 0;
+  for ($x = 0; $x <= count($array); $x++) {
+    if($array[$x]["application"] == "A") $A++;
+    if($array[$x]["application"] == "B") $B++;
+  }
+  if($A <= $B){
     header("Location: /Examensarbete/A/");
   }else{
     header("Location: /Examensarbete/B/");
